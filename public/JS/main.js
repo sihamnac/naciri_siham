25f4ea709389a18eee9e3025b22c2603f61cb157
Siham <n.siham@hotmail.be>
2022-06-17 10:15:59 +0200

    first push

---
+++ public/JS/main.js
@@ -0,0 +1,14 @@
+function fontfamily() {
+    var paragraph = document.getElementById("para");
+    paragraph.style.fontFamily = "roboto";
+} 
+
+
+let taillebackground = document.querySelector("button2");
+taillebackground.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#EEECE4";
+    
+})
+// il faut encore faire en sorte que la taille change pour le même bouton
+
+
